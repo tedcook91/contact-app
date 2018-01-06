d8fff17da2d9b8e5fce8570085db0d750cb3b396
captainangus <27704548+CaptainAngus@users.noreply.github.com>
2018-01-06 11:51:49 -0500

    components and containers

---
+++ react/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ContactsContainer from './containers/ContactsContainer';
+
+const App = props =>
+  <div>
+    <ContactsContainer />
+  </div>
+
+export default App;
---
+++ react/src/components/ContactForm.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react'
+import TextInputField from "../components/TextInputField";
+
+class ContactForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName:"",
+      lastName:"",
+      emailAddress:"",
+      phoneNumber:"",
+      companyName:""
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleClearForm = this.handleClearForm.bind(this)
+  }
+
+  handleChange(event) {
+    let value = event.target.value;
+    let name = event.target.name;
+    this.setState({ [name]: value })
+  }
+
+  handleClearForm(event) {
+    event.preventDefault();
+    this.setState({
+      firstName:"",
+      lastName:"",
+      emailAddress:"",
+      phoneNumber:"",
+      companyName:""
+    })
+  };
+
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const formPayload = {
+      firstName:"",
+      lastName:"",
+      emailAddress:"",
+      phoneNumber:"",
+      companyName:""
+    }
+    this.props.addContact(formPayload)
+    this.handleClearForm(event);
+  }
+
+  render() {
+    return(
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <TextInputField
+            content={this.state.firstName}
+            label="First Name"
+            name="firstName"
+            handleChange={this.handleChange}
+          />
+          <TextInputField
+            content={this.state.lastName}
+            label="Last Name"
+            name="lastName"
+            handleChange={this.handleChange}
+          />
+          <TextInputField
+            content={this.state.emailAddress}
+            label="Email Address"
+            name="emailAddress"
+            handleChange={this.handleChange}
+          />
+          <TextInputField
+            content={this.state.phoneNumber}
+            label="Phone Number"
+            name="phoneNumber"
+            handleChange={this.handleChange}
+          />
+          <TextInputField
+            content={this.state.companyName}
+            label="Company Name"
+            name="companyName"
+            handleChange={this.handleChange}
+          />
+          <div>
+            <input className="button" type="submit" value="Submit" />
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default ContactForm;
