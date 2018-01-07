1c5e68085ef01867f7004656bc9a6cf563b30d30
captainangus <edward.w.cook@gmail.com>
2018-01-06 19:07:20 -0500

    rails work

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,28 +1,3 @@
-class ContactsController < ApplicationController
-
-  def index
-    @contacts = Contact.all
-  end
-
-  def new
-    @contact = Contact.new
-  end
-
-  def create
-    @contact = Contact.new(contact_params)
-
-    if @contact.save
-      notice: 'Contact was successfully created.'
-    else
-      render action: 'new'
-    end
-  end
-
-  def destroy
-    @contact.destroy
-  end
-
-  def contact_params
-    params.require(:contact).permit(:firstName, :lastName, :emailAddress, :phoneNumber, :companyName)
-  end
-end
+# class ContactsController < ApplicationController
+#
+# end
